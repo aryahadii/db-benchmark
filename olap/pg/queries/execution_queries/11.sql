f5eade26800a8503f191bc7c41782a5d21784385
Arya Hadi <arya.hadi97@gmail.com>
2019-01-18 23:32:37 +0330

    fix fraction

--- olap/pg/queries/execution_queries/11.sql
+++ olap/pg/queries/execution_queries/11.sql
@@ -13,7 +13,7 @@ group by
 	ps_partkey having
 		sum(ps_supplycost * ps_availqty) > (
 			select
-				sum(ps_supplycost * ps_availqty) * 2
+				sum(ps_supplycost * ps_availqty) * 0.0001
 			from
 				partsupp,
 				supplier,
