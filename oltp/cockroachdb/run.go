b8c347f10eb6ed936e36d6998510bd2583da3559
Arya Hadi <arya.hadi97@gmail.com>
2019-01-31 19:44:35 +0330

    add cockroachdb runner script

---
+++ oltp/cockroachdb/run.go
@@ -0,0 +1,97 @@
+#!/usr/bin/env gorun
+
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/exec"
+	"strconv"
+)
+
+const (
+	containerName = "cockroachdb_roach1_1"
+	statsPath     = "results/cdb_%d.stat"
+	outputPath    = "results/cdb_%d.out"
+)
+
+var (
+	warehouses []int
+)
+
+func main() {
+	parseArgs()
+	run()
+}
+
+func parseArgs() {
+	if len(os.Args) < 3 {
+		log.Fatalln("not enough args")
+	}
+
+	for _, warehouse := range os.Args[1:] {
+		arg, err := strconv.ParseInt(warehouse, 10, 32)
+		if err != nil {
+			log.Fatalln("cannot parse warehouse arg")
+		}
+		warehouses = append(warehouses, int(arg))
+	}
+}
+
+func run() {
+	for _, warehouse := range warehouses {
+		if err := runQuery(warehouse); err != nil {
+			log.Printf("error while running for %d warehouses: %v", warehouses, err)
+		}
+	}
+}
+
+func runQuery(warehouse int) error {
+	ctx, monitoringCancelFunc := context.WithCancel(context.Background())
+	defer monitoringCancelFunc()
+	go monitorSystem(ctx, warehouse)
+
+	return runWorkload(warehouse)
+}
+
+func runWorkload(warehouse int) error {
+	args := []string{
+		"exec",
+		"-t",
+		containerName,
+		"bash",
+		"-c",
+		fmt.Sprintf("cockroach workload init tpcc --drop --warehouses %d", warehouse),
+	}
+	cmd := exec.Command("docker", args...)
+	_, err := cmd.CombinedOutput()
+	log.Printf("%d warehouses loaded completely", warehouse)
+
+	args = []string{
+		"exec",
+		"-t",
+		containerName,
+		"bash",
+		"-c",
+		fmt.Sprintf("cockroach workload run tpcc --warehouses %d", warehouse),
+	}
+	cmd = exec.Command("docker", args...)
+	output, err := cmd.CombinedOutput()
+	outputFilePath := fmt.Sprintf(outputPath, warehouse)
+	ioutil.WriteFile(outputFilePath, output, os.ModePerm)
+	return err
+}
+
+func monitorSystem(ctx context.Context, warehouse int) error {
+	outputFilePath := fmt.Sprintf(statsPath, warehouse)
+	args := []string{
+		"-lcmdrsyTt",
+		"--full",
+		"--output",
+		outputFilePath,
+	}
+	return exec.CommandContext(ctx, "dstat", args...).Run()
+}
